0a2aefbb31d4f491c2a4dcd05bc3df4588f01f51
Ezequiel Mareco <ezequielmareco33@gmail.com>
2025-09-10 21:02:54 -0300

    agrego Modificación y Listado a la clase DaoCategoria

--- src/dao/DaoCategoria.java
+++ src/dao/DaoCategoria.java
@@ -57,5 +57,55 @@ public class DaoCategoria {
 	}
 		
 
+	public int modificarCategoria(Categoria categoria) {
+		String query = "UPDATE Categorias SET Nombre = ?, IdCategoria = ? WHERE IdCategoria = ?";		
+		Connection cn = null;
+		
+		int filas = 0;
+		
+		try {
+			cn = DriverManager.getConnection(host+dbName,user,pass);
+			PreparedStatement pS = cn.prepareStatement(query);
+			pS.setString(1, categoria.getNombre());
+			pS.setInt(2, categoria.getId());
+			pS.setInt(3, categoria.getId());
+			pS.executeUpdate();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return filas;
+	}
+	
+	public ArrayList<Categoria> listarCategorias(Categoria categoria) {
+		
+		ArrayList<Categoria> lCategorias = new ArrayList<Categoria>();
+		
+		String query = "SELECT * FROM Categorias";
+		Connection cn = null;
+
+		try {
+			cn = DriverManager.getConnection(host+dbName,user,pass);
+			
+			Statement st = cn.createStatement();
+			ResultSet rS = st.executeQuery(query);
+
+			while (rS.next()) 
+			{
+				Categoria cat = new Categoria();
+				cat.setId(rS.getInt("IdCategoria"));
+	            cat.setNombre(rS.getString("Nombre"));
+	            lCategorias.add(cat);
+	            
+	            System.out.println("ID: " + cat.getId() + ", Nombre: " + cat.getNombre());
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return lCategorias;
+	}
 	
 }
